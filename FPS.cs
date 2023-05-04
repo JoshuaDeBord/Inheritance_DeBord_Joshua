21f953d1410dba25316f7d5509de706c22c36eb3
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-05-04 13:18:51 -0500

    Initial commit

---
+++ FPS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_DeBord_Joshua
+{
+    class FPS : Game
+    {
+
+        public FPS(string esrbParam, string titleParam) : base(esrbParam, titleParam)
+        {
+            Esrb = esrbParam;
+            Title = titleParam;
+        }
+        public override string Describe()
+        {
+            return $"{Title} is a fps game rated {Esrb}!";
+        }
+
+
+
+
+    }
+}
